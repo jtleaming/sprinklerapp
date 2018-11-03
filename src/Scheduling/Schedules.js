1942ad6fca9975821003818574b7ce618def98ef
Justin Leaming <jtleaming@outlook.com>
2018-11-03 13:36:05 -0600

    Get shceudles changes

--- src/Scheduling/Schedules.js
+++ src/Scheduling/Schedules.js
@@ -6,6 +6,7 @@ import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import Notifications from 'react-notify-toast';
 import sprinkler from '../sprinkler.svg';
+import Axios from 'axios';
 
 
 class Schedules extends Component {
@@ -14,11 +15,19 @@ class Schedules extends Component {
         this.state = Schedules.setInitialState();
     }
 
-    static setInitialState = () =>({
-        schedule: null
+    static setInitialState = () => ({
+        schedule: []
     });
 
-    toggleSchedule = (scheduleName) =>{
+    componentDidMount() {
+        Axios.get('http://localhost:5005/schedule')
+            .then(resposne => {
+                const schedule = resposne.data;
+                this.setState({schedule: schedule});
+            });
+    }
+
+    toggleSchedule = (Id) => {
         var schedule = JSON.parse(localStorage.getItem(scheduleName));
 
         schedule.Run ? schedule.Run = false : schedule.Run = true;
@@ -32,26 +41,24 @@ class Schedules extends Component {
         var scheduleProps = JSON.parse(localStorage.getItem(name));
         var schedule = this.state.schedule;
 
-        schedule = 
-        <div>
-            <Schedule createRows={true} rowConfig={scheduleProps}> </Schedule>
-            <div style={{textAlign: 'center'}}>
-                {scheduleProps.Run ?
-                    <Button id='clear-button' onClick={() => this.toggleSchedule(name)}>Set to not Run</Button>:
-                    <Button id='save-button' onClick={() => this.toggleSchedule(name)}>Set to Run</Button>
-                }
-                <Button>Edit Schedule</Button>
-                <Button>Delete Schedule</Button>
-            </div>
-        </div>;
+        schedule =
+            <div>
+                <Schedule createRows={true} rowConfig={scheduleProps}> </Schedule>
+                <div style={{ textAlign: 'center' }}>
+                    {scheduleProps.Run ?
+                        <Button id='clear-button' onClick={() => this.toggleSchedule(name)}>Set to not Run</Button> :
+                        <Button id='save-button' onClick={() => this.toggleSchedule(name)}>Set to Run</Button>
+                    }
+                    <Button>Edit Schedule</Button>
+                    <Button>Delete Schedule</Button>
+                </div>
+            </div>;
         this.setState({
             schedule
         });
     }
 
     render() {
-        let zonesToSelect = Object.keys(JSON.parse(localStorage.Zones));
-        zonesToSelect.shift();
         return (
             <div>
                 <div className="App">
@@ -69,12 +76,15 @@ class Schedules extends Component {
                     </header>
                 </div>
                 {
-                    Object.keys(localStorage).map((name, i) =>
-                        name.includes('Schedule') &&
-                        <Button key={i} onClick={() => this.expandSchedule(name)}>{name}</Button>
+                    // Object.keys(localStorage).map((name, i) =>
+                    //     name.includes('Schedule') &&
+                    //     <Button key={i} onClick={() => this.expandSchedule(name)}>{name}</Button>
+                    // )
+                    this.state.schedule.map((sch, i) => 
+                        <Button key={i} onClick={console.log(sch)}> Schedule {sch.Id}</Button>
                     )
                 }
-                {this.state.schedule}
+                {/* {this.state.schedule} */}
             </div>
         );
     }
